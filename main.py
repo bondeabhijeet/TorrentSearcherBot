41fb888583f6ec885fbbd21d98d4f493f3132397
kali <Abhijeet>
2021-10-14 01:14:31 +0530

    updated for 1337x

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 
 from BotModules import TorrentSearcher as TorrentSearcher
 from BotModules import bothelp as bothelp
+from BotModules import o1337xSearcher as o1337xSearcher
 
 # Reading the config.txt file to get configuration details
 with open('config.json') as json_file:
@@ -31,6 +32,15 @@ def BotHelp(update, context):
     ChatID = update.message.chat_id
     context.bot.sendMessage(chat_id = ChatID, text = HelpMessage, parse_mode = ParseMode.HTML)
     
+def o1337x(update, context):
+
+    RecievedMsg = update.message.text
+
+    CommandToReplace = ConfigData['COMMANDS']['o1337x']
+    NoOf1337xResults = int(ConfigData['NoOf1337xResults'])
+    MessageID = update.message.message_id
+
+    o1337xSearcher.Get1337x(RecievedMsg, CommandToReplace, NoOf1337xResults, MessageID, update, context)
 
 def main():
     updater = Updater(ConfigData['API_KEY'], use_context=True)
@@ -38,8 +48,11 @@ def main():
 
     dp.add_handler(CommandHandler(f"{ConfigData['COMMANDS']['Yts']}", yts_command))
     dp.add_handler(CommandHandler(f"{ConfigData['COMMANDS']['Help']}", BotHelp))
+    dp.add_handler(CommandHandler(f"{ConfigData['COMMANDS']['o1337x']}", o1337x))
 
     updater.start_polling()
+    print("\n[√] BOT STARTED SUCCESSFULLY [√]\n")
     updater.idle()
+    
 
 main()
