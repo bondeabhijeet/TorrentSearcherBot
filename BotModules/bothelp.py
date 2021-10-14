ab16712291e2e1e788cbf47e8c1ced41a9f7895a
bondeabhijeet <bondeabhijeetunderveil@gmail.com>
2021-10-14 09:16:42 +0530

    1337x Help added

--- BotModules/bothelp.py
+++ BotModules/bothelp.py
@@ -9,8 +9,12 @@ def BotHelpMessage(Commands):
 >> Will get this message.
 
 /{Commands['Yts']} <code>[ movie name ]</code> | <code>[ quality of the movie ]</code>
->> Will get the avaliable Direct Download Links of torrents for the specified movie name with mentioned quality
-<b>NOTE: by default quality is 1080p.</b>"""
+>> Will get the Direct Download Links of torrents avaliable for the specified movie name with mentioned quality
+<b>NOTE: by default quality is 1080p.</b>
+
+/{Commands['o1337x']} <code>[ Search query ]
+>> Will get the Torrent details and their magnet link avaliable for the specified Search query
+"""
 
     print("Help requested!!!")
     return Msg
