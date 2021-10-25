560aff24c35a3a0a959a7ebd4812084866ddb51a
Abhijeet Bonde <bondeabhijeetunderveil@gmail.com>
2021-10-25 12:55:28 +0530

    minor big fix

--- BotModules/bothelp.py
+++ BotModules/bothelp.py
@@ -12,7 +12,7 @@ def BotHelpMessage(Commands):
 >> Will get the Direct Download Links of torrents avaliable for the specified movie name with mentioned quality
 <b>NOTE: by default quality is 1080p.</b>
 
-/{Commands['o1337x']} <code>[ Search query ]
+/{Commands['o1337x']} <code>[ Search query ]</code>
 >> Will get the Torrent details and their magnet link avaliable for the specified Search query
 """
 
