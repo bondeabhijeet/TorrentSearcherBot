24c4009a2473e37a1d17750f440bfec545c231a0
Abhijeet Bonde <bondeabhijeetunderveil@gmail.com>
2021-11-13 01:25:25 +0530

    Nyaasi updated -> minor bug

--- BotModules/NyaasiSearcher.py
+++ BotModules/NyaasiSearcher.py
@@ -1,4 +1,5 @@
 import requests
+import time
 from bs4 import BeautifulSoup
 from telegram.parsemode import ParseMode
 
@@ -22,6 +23,7 @@ def SendMessage(update, context, MsgText, MessageID):
     Chat_ID = update.message.chat_id
 
     msg = context.bot.sendMessage(chat_id = Chat_ID, text = MsgText, reply_to_message_id=MessageID, parse_mode = ParseMode.HTML)
+    time.sleep(0.8)
     return
 
 def CreateMessage(Name, Size, Seeders, Leechers, DownloadCompleted, MagnetLink):
