7d8d63decdd93e1129ef526b1379f39140e1241a
Abhijeet Bonde <bondeabhijeetunderveil@gmail.com>
2021-11-12 11:49:12 +0530

    fixed minor bugs

--- BotModules/bothelp.py
+++ BotModules/bothelp.py
@@ -9,11 +9,15 @@ def BotHelpMessage(Commands):
 >> Will get this message.
 
 /{Commands['Yts']} <code>[ movie name ]</code> | <code>[ quality of the movie ]</code>
->> Will get the Direct Download Links of torrents avaliable for the specified movie name with mentioned quality
 <b>NOTE: by default quality is 1080p.</b>
+>> Will get the Direct Download Links of torrents avaliable for the specified movie name with mentioned quality
 
 /{Commands['o1337x']} <code>[ Search query ]</code>
->> Will get the Torrent details and their magnet link avaliable for the specified Search query
+>> Will get the Torrent details and their magnet link avaliable for the specified Search query.
+
+/{Commands['Nyaasi']} <code>[ Search query ]</code>
+<b>NOTE: This website is mainly for ANIME Torrents.</b>
+>> Will get the Torrent details and their magnet link avaliable for the specified Search query.
 """
 
     print("Help requested!!!")
